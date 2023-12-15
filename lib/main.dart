e75b877fe1183939ba1c89b112f7e1e8f94d3dac
mcalisadeghi <ali.ke970@gmail.com>
2023-12-15 18:41:22 +0330

    add IconButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Webview App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const WebviewApp(),
-          );
+    );
   }
 }
+
 class WebviewApp extends StatefulWidget {
   const WebviewApp({super.key});
 
@@ -28,8 +31,76 @@ class WebviewApp extends StatefulWidget {
 }
 
 class _WebviewAppState extends State<WebviewApp> {
+  late final WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse(
+          'https://google.com',
+        ),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Web view',
+        ),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final messenger = ScaffoldMessenger.of(context);
+              if (await controller.canGoBack()) {
+                await controller.goBack();
+              } else {
+                messenger.showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'No Back History Found',
+                    ),
+                  ),
+                );
+                return;
+              }
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+            ),
+          ),
+          IconButton(
+            onPressed: () async {
+              final messenger = ScaffoldMessenger.of(context);
+              if (await controller.canGoForward()) {
+                await controller.goForward();
+              } else {
+                messenger.showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'No Forward History Found',
+                    ),
+                  ),
+                );
+                return;
+              }
+            },
+            icon: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              controller.reload();
+            },
+            icon: Icon(
+              Icons.reply,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
